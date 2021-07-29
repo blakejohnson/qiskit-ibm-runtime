bd595fb5365dd3f2a2839fb1d0291aa518f9e6e8
Julien Gacon <gaconju@gmail.com>
2021-07-29 15:31:48 +0200

    add tests on runtime script & programs

---
+++ test/qaoa/test_program.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+from qiskit import IBMQ, Aer
+from qiskit.algorithms import NumPyMinimumEigensolver, QAOA
+from qiskit.algorithms.optimizers import SPSA
+from qiskit.opflow import Z, I
+
+from qiskit_optimization.runtime import QAOAProgram
+
+hamiltonian = (Z ^ Z ^ I ^ I) + (I ^ Z ^ Z ^ I) + (Z ^ I ^ I ^ Z)
+reference = NumPyMinimumEigensolver().compute_minimum_eigenvalue(hamiltonian)
+print("Exact:", reference.eigenvalue)
+
+reps = 2
+initial_point = np.random.random(2 * reps)
+optimizer = SPSA(maxiter=300, learning_rate=0.01, perturbation=0.01)
+
+simulator = Aer.get_backend("qasm_simulator")
+local_qaoa = QAOA(
+    optimizer=optimizer,
+    reps=reps,
+    initial_point=initial_point,
+    quantum_instance=simulator
+)
+local_result = local_qaoa.compute_minimum_eigenvalue(hamiltonian)
+print("Local simulator:", local_result.eigenvalue)
+
+IBMQ.load_account()
+provider = IBMQ.get_provider(hub="ibm-q", group="open", project="main")
+backend = provider.get_backend("ibmq_qasm_simulator")
+
+qaoa = QAOAProgram(
+    optimizer=optimizer,
+    reps=reps,
+    initial_point=initial_point,
+    provider=provider,
+    backend=backend,
+)
+result = qaoa.compute_minimum_eigenvalue(hamiltonian)
+print("Runtime:", result.eigenvalue)
+
---
+++ test/vqe/test_script.py
@@ -0,0 +1,39 @@
+import numpy as np
+from qiskit import IBMQ
+from qiskit.algorithms import NumPyMinimumEigensolver
+from qiskit.algorithms.optimizers import SPSA
+from qiskit.circuit.library import EfficientSU2
+from qiskit.opflow import X, Z, I
+
+spin_coupling = (Z ^ Z ^ I) + (I ^ Z ^ Z)
+transverse_field = (X ^ I ^ I) + (I ^ X ^ I) + (I ^ I ^ X)
+hamiltonian = -0.5 * (spin_coupling + 0.5 * transverse_field)
+
+reference = NumPyMinimumEigensolver().compute_minimum_eigenvalue(hamiltonian)
+print("Exact result:", reference.eigenvalue)
+
+ansatz = EfficientSU2(3, entanglement="linear", reps=3)
+initial_point = np.random.random(ansatz.num_parameters)
+optimizer = SPSA(maxiter=300)
+
+inputs = {
+    "operator": hamiltonian,
+    "ansatz": ansatz,
+    "initial_point": initial_point,
+    "optimizer": optimizer
+}
+
+options = {"backend_name": "ibmq_qasm_simulator"}
+
+IBMQ.load_account()
+provider = IBMQ.get_provider(hub="ibm-q", group="open", project="main")
+
+job = provider.runtime.run(
+    program_id="vqe",
+    inputs=inputs,
+    options=options
+)
+
+result = job.result()
+
+print("Runtime:", result["eigenvalue"])
