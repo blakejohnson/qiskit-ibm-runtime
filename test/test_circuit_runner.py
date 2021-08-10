a823c6d145f9275848a69796183310baab014ba3
Lucy-Xing <Lucy.Xing@ibm.com>
2021-08-10 13:30:43 -0400

    update circuit  runner test

--- test/test_circuit_runner.py
+++ test/test_circuit_runner.py
@@ -1,18 +1,25 @@
+from qiskit.providers import provider
 from qiskit.providers.ibmq import RunnerResult
 from qiskit import IBMQ, QuantumCircuit
 from unittest import TestCase
 
 class TestCircuitRunner(TestCase):
-    def test_circuit_runner(self):
+    """Test circuit_runner"""
 
-        provider = IBMQ.load_account()
+    def setUp(self) -> None:
+        """Test case setup."""
+        self.provider = IBMQ.load_account()
         N = 6
         qc = QuantumCircuit(N)
         qc.x(range(0, N))
         qc.h(range(0, N))
+        self.qc = qc
+        
+    def test_no_error_mitigation(self):
+
 
         program_inputs = {
-            'circuits': qc,
+            'circuits': self.qc,
             'shots': 2048,
             'optimization_level': 0,
             'initial_layout': [0,1,4,7,10,12],
@@ -21,12 +28,33 @@ class TestCircuitRunner(TestCase):
 
         options = {'backend_name': "ibmq_qasm_simulator"}
 
-        job = provider.runtime.run(program_id="circuit-runner",
+        job = self.provider.runtime.run(program_id="circuit-runner",
                                     options=options,
                                     inputs=program_inputs,
                                     result_decoder=RunnerResult
                                     )
         result = job.result()
-        print("CIRCUIT_RUNNER")
         print("Runtime:", result)
-        print("status", job.status())
+        expected_status = "JobStatus.DONE"
+        self.assertEqual(str(job.status), expected_status)
+    
+    def test_error_mitigation(self):
+        program_inputs = {
+            'circuits': self.qc,
+            'shots': 2048,
+            'optimization_level': 0,
+            'initial_layout': [0,1,4,7,10,12],
+            'measurement_error_mitigation': True
+        }
+
+        options = {'backend_name': "ibmq_qasm_simulator"}
+
+        job = self.provider.runtime.run(program_id="circuit-runner",
+                                    options=options,
+                                    inputs=program_inputs,
+                                    result_decoder=RunnerResult
+                                    )
+        result = job.result()
+        print("Runtime:", result)
+        expected_status = "JobStatus.DONE"
+        self.assertEqual(str(job.status), expected_status)
