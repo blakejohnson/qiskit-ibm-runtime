14747471447225ef484ff140a369a2640cb24162
Lucy-Xing <Lucy.Xing@ibm.com>
2021-08-17 13:24:18 -0400

    format file

--- test/test_sample_program.py
+++ test/test_sample_program.py
@@ -3,6 +3,7 @@ from qiskit import IBMQ
 import os
 from unittest import TestCase, SkipTest
 
+
 class MethodCallLogger:
     def __init__(self, func):
         self.func = func
@@ -11,13 +12,15 @@ class MethodCallLogger:
     def __call__(self, *args):
         self.func(*args)
         self.call_count += 1
+
+
 class TestSampleProgram(TestCase):
     """Test sample_program."""
     def setUp(self) -> None:
         """Test case setup."""
         def interim_result_callback(job_id, interim_result):
             pass
-        interim_result_callback= MethodCallLogger(interim_result_callback)
+        interim_result_callback = MethodCallLogger(interim_result_callback)
         self.interim_result_callback = interim_result_callback
         backend_name = os.getenv("QISKIT_IBM_DEVICE", None)
         if not backend_name:
@@ -32,10 +35,10 @@ class TestSampleProgram(TestCase):
         }
         options = {"backend_name": self.backend_name}
         job = provider.runtime.run(program_id="sample-program",
-                                options=options,
-                                inputs=input,
-                                callback=self.interim_result_callback
-                                )
+                                   options=options,
+                                   inputs=input,
+                                   callback=self.interim_result_callback
+                                   )
         expected_result = "All done!"
         self.assertEqual(job.result(), expected_result)
         self.assertEqual(self.interim_result_callback.call_count, input["iterations"])
