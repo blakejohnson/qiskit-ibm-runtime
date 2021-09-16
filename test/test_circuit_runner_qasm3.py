a7a380cae60856642a4c1c03bf0a381a60d6a120
Lucy-Xing <Lucy.Xing@ibm.com>
2021-09-16 18:06:31 -0400

    skip test

--- test/test_circuit_runner_qasm3.py
+++ test/test_circuit_runner_qasm3.py
@@ -51,6 +51,7 @@ class TestCircuitRunnerQASM3(BaseTestCase):
         h input;
         output = measure input;'''
 
+    @skip("Skip until backend supports qasm3")
     def test_circuit_runner_qasm3(self):
         """Test circuit_runner_qasm3 program."""
         program_inputs = {
