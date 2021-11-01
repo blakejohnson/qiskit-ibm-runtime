41c4b0e77d8b0463bc6089ba6f4ef40eafc37456
JESSIE YU <jessieyu@us.ibm.com>
2021-11-01 16:02:26 -0400

    Update qasm3 runner to use simulator (#76)
    * update qasm3 runner
    
    * add qasm3 encoder

---
+++ runtime/qasm3_runner.py
@@ -0,0 +1,251 @@
+# Circuit runner QASM3 runtime program
+
+from time import perf_counter
+
+from qiskit.circuit.quantumcircuit import QuantumCircuit
+from qiskit.compiler import transpile
+from qiskit.result import marginal_counts
+from qiskit.providers.ibmq.runtime.utils import RuntimeEncoder
+import mthree
+import numpy as np
+
+try:
+    from qiskit.qasm3 import Exporter
+    HAS_QASM3_EXPORTER = True
+except ImportError:
+    HAS_QASM3_EXPORTER = False
+
+QASM3_SIM_NAME = "simulator_qasm3"
+
+
+class Qasm3Encoder(RuntimeEncoder):
+
+    def default(self, obj):  # pylint: disable=arguments-differ
+        if isinstance(obj, np.bool_):
+            return bool(obj)
+        return super().default(obj)
+
+
+def main(backend, user_messenger,
+         circuits,
+         transpiler_config=None,
+         exporter_config=None,
+         run_config=None,
+         qasm3_args=None,
+         skip_transpilation=False,
+         use_measurement_mitigation=False
+         ):
+    """Execute
+
+    Args:
+        backend: Backend to execute circuits on.
+        user_messenger: Used to communicate with the users.
+        circuits: Circuits to execute.
+        transpiler_config: Transpiler configurations.
+        exporter_config: QASM3 exporter configurations.
+        run_config: Execution time configurations.
+        qasm3_args: Arguments to pass to the QASM3 program loop.
+        skip_transpilation: Skip transpiling of circuits.
+        use_measurement_mitigation: Whether to perform measurement error mitigation.
+
+    Returns:
+        Program result.
+    """
+
+    if circuits and not isinstance(circuits, list):
+        circuits = [circuits]
+
+    if not circuits or \
+        (not all(isinstance(circ, QuantumCircuit) for circ in circuits) and
+            not all(isinstance(circ, str) for circ in circuits)):
+        raise ValueError('Circuit need to be of type QuantumCircuit or str and \
+            circuit types need to be consistent in a list of circuits.')
+
+    # TODO Better validation once we can query for input_allowed
+    if backend.configuration().simulator and backend.name() != QASM3_SIM_NAME:
+        raise ValueError("This backend does not support QASM3")
+
+    is_qc = isinstance(circuits[0], QuantumCircuit)
+
+    if use_measurement_mitigation and (not is_qc or backend.name() != QASM3_SIM_NAME):
+        raise NotImplementedError("Measurement error mitigation is only supported for "
+                                  "QuantumCircuit inputs and non-simulator backends.")
+
+    if is_qc and not HAS_QASM3_EXPORTER:
+        raise RuntimeError(
+            "You are not authorized to use this program with QuantumCircuit inputs.")
+
+    run_config = run_config or {}
+
+    if is_qc:
+        if not skip_transpilation:
+            # Transpile the circuits using given transpile options
+            transpiler_config = transpiler_config or {}
+            circuits = transpile(circuits, backend=backend, **transpiler_config)
+
+        # Convert circuits to qasm3
+        qasm3_strs = []
+        qasm3_metadata = []
+        for circ in circuits:
+            qasm3_strs.append(Exporter(**exporter_config).dumps(circ))
+            qasm3_metadata.append(get_circuit_metadata(circ))
+
+        payload = qasm3_strs
+        run_config["qasm3_metadata"] = qasm3_metadata
+
+        if use_measurement_mitigation:
+            # get final meas mappings
+            mappings = []
+            all_meas_qubits = []
+            for idx, circ in enumerate(circuits):
+                mappings.append(final_measurement_mapping(circ))
+                all_meas_qubits.extend(list(mappings[idx].keys()))
+
+            # Collect set of active measured qubits over which to
+            # mitigate.
+            all_meas_qubits = list(set(all_meas_qubits))
+
+            # Get an M3 mitigator and calibrate over all measured qubits
+            mit = mthree.M3Mitigation(backend)
+            mit.tensored_cals_from_system(all_meas_qubits)
+    else:
+        payload = circuits
+
+    if backend.name() == QASM3_SIM_NAME:
+        if len(payload) > 1:
+            raise ValueError("QASM3 simulator only supports a single circuit.")
+        result = backend.run(payload[0],
+                             args=qasm3_args,
+                             shots=run_config.get("shots", None))
+        user_messenger.publish(result, final=True, encoder=Qasm3Encoder)
+        return
+
+    result = backend.run(payload, **run_config).result()
+
+    # Do the actual mitigation here
+    if use_measurement_mitigation:
+        quasi_probs = []
+        mit_times = []
+        for idx, circ in enumerate(circuits):
+            num_cbits = circ.num_clbits
+            num_measured_bits = len(mappings[idx])
+            raw_counts = result.get_counts(idx)
+            # check if more bits than measured so need to marginalize
+            if num_cbits > num_measured_bits:
+                raw_counts = marginal_counts(raw_counts,
+                                             list(mappings[idx].values()))
+            _qubits = list(mappings[idx].keys())
+            start_time = perf_counter()
+            quasi = mit.apply_correction(raw_counts, _qubits)
+            stop_time = perf_counter()
+            mit_times.append(stop_time-start_time)
+            # Convert quasi dist with bitstrings to hex version and append
+            quasi_probs.append(quasi_to_hex(quasi))
+
+        # Attach to results.
+        for idx, res in enumerate(result.results):
+            res.data.quasiprobabilities = quasi_probs[idx]
+            res.data._data_attributes.append('quasiprobabilities')
+            res.header.final_measurement_mapping = mappings[idx]
+            res.header.measurement_mitigation_time = mit_times[idx]
+
+    return result.to_dict()
+
+
+def get_circuit_metadata(circuit: QuantumCircuit):
+    """Get the circuit metadata."""
+
+    # header data
+    num_qubits = 0
+    memory_slots = 0
+    qubit_labels = []
+    clbit_labels = []
+
+    qreg_sizes = []
+    creg_sizes = []
+    for qreg in circuit.qregs:
+        qreg_sizes.append([qreg.name, qreg.size])
+        for j in range(qreg.size):
+            qubit_labels.append([qreg.name, j])
+        num_qubits += qreg.size
+    for creg in circuit.cregs:
+        creg_sizes.append([creg.name, creg.size])
+        for j in range(creg.size):
+            clbit_labels.append([creg.name, j])
+        memory_slots += creg.size
+
+    return {
+        "qubit_labels": qubit_labels,
+        "n_qubits": num_qubits,
+        "qreg_sizes": qreg_sizes,
+        "clbit_labels": clbit_labels,
+        "memory_slots": memory_slots,
+        "creg_sizes": creg_sizes,
+        "name": circuit.name,
+        "global_phase": float(circuit.global_phase),
+        "metadata": circuit.metadata or {}
+    }
+
+
+def final_measurement_mapping(qc):
+    """Returns the final measurement mapping for a circuit that
+    has been transpiled (flattened registers) or has flat registers.
+    
+    Parameters:
+        qc (QuantumCircuit): Input quantum circuit.
+    
+    Returns:
+        dict: Mapping of qubits to classical bits for final measurements.
+
+    Raises:
+        ValueError: More than one quantum or classical register.
+    """
+    if len(qc.qregs) > 1 or len(qc.qregs) > 1:
+        raise ValueError('Number of quantum or classical registers is greater than one.')
+    num_qubits = qc.num_qubits
+    num_clbits = qc.num_clbits
+    active_qubits = list(range(num_qubits))
+    active_cbits = list(range(num_clbits))
+    qmap = []
+    cmap = []
+    for item in qc._data[::-1]:
+        if item[0].name == 'measure':
+            cbit = item[2][0].index
+            qbit = item[1][0].index
+            if cbit in active_cbits and qbit in active_qubits:
+                qmap.append(qbit)
+                cmap.append(cbit)
+                active_cbits.remove(cbit)
+                active_qubits.remove(qbit)
+        elif item[0].name != 'barrier':
+            for qq in item[1]:
+                if qq.index in active_qubits:
+                    active_qubits.remove(qq.index)
+
+        if not len(active_cbits) or not len(active_qubits):
+            break
+    if cmap and qmap:
+        mapping = {}
+        for idx, qubit in enumerate(qmap):
+            mapping[qubit] = cmap[idx]
+    else:
+        raise ValueError('Measurement not found in circuits.')
+
+    # Sort so that classical bits are in numeric order low->high.
+    mapping = dict(sorted(mapping.items(), key=lambda item: item[1])) 
+    return mapping
+
+
+def quasi_to_hex(qp):
+    """Converts a quasi-prob dict with bitstrings to hex
+
+    Parameters:
+        qp (QuasiDistribution): Input quasi dict
+
+    Returns:
+        dict: hex dict.
+    """
+    hex_quasi = {}
+    for key, val in qp.items():
+        hex_quasi[hex(int(key, 2))] = val     
+    return hex_quasi  
--- test/test_circuit_runner_qasm3.py
+++ test/test_circuit_runner_qasm3.py
@@ -1,10 +1,35 @@
-from unittest import skip
+from unittest import skip, skipIf
 
 from qiskit.providers.ibmq import RunnerResult
 from qiskit.providers.jobstatus import JobStatus
 
 from .decorator import get_provider_and_backend
 from .base_testcase import BaseTestCase
+
+
+QASM3_STR = """
+OPENQASM 3;
+include "stdgates.inc";
+bit[2] qc;
+qubit[2] qr;
+
+h qr[0];
+cx qr[0], qr[1];
+qc[0] = measure qr[0];
+qc[1] = measure qr[1];   
+"""
+
+QASM3_STR_WITH_ARGS = """
+OPENQASM 3;
+include "stdgates.inc";
+input int[32] flip;
+output bit result;
+qubit q;
+
+reset q;
+if (flip == 1) x q;
+result = measure q;
+"""
 
 
 class TestCircuitRunnerQASM3(BaseTestCase):
@@ -15,57 +40,113 @@ class TestCircuitRunnerQASM3(BaseTestCase):
     def setUpClass(cls, provider, backend_name):
         """Class setup."""
         super().setUpClass()
-        cls.provider = provider
+        cls.runtime = provider.runtime
         cls.backend_name = backend_name
-
-    def setUp(self) -> None:
-        """Test case setup."""
-        self.qasm3_str ='''OPENQASM 3;
-        include "stdgates.inc";
-
-        def segment qubit[2] anc, qubit psi -> bit[2] {
-        bit[2] b;
-        reset anc;
-        h anc;
-        ccx anc[0], anc[1], psi;
-        s psi;
-        ccx anc[0], anc[1], psi;
-        z psi;
-        h anc;
-        measure anc -> b;
-        return b;
-        }
-
-        qubit input;
-        qubit[2] ancilla;
-        bit[2] flags = "11";
-        bit output;
-
-        reset input;
-        h input;
-
-        while(int(flags) != 0) {
-        flags = segment ancilla, input;
-        }
-        rz(pi - arccos(3 / 5)) input;
-        h input;
-        output = measure input;'''
+        cls.program_id = cls._find_program_id("qasm3-runner")
 
     @skip("Skip until backend supports qasm3")
-    def test_circuit_runner_qasm3(self):
-        """Test circuit_runner_qasm3 program."""
+    def test_circuit_runner_qasm3_real(self):
+        """Test circuit_runner_qasm3 program on a real device."""
         program_inputs = {
-            'circuits': self.qasm3_str,
+            'circuits': QASM3_STR,
             'use_qasm3': True,
         }
 
         options = {"backend_name": self.backend_name}
 
-        job = self.provider.runtime.run(program_id="circuit-runner-qasm3",
-                                        options=options,
-                                        inputs=program_inputs,
-                                        result_decoder=RunnerResult,
-                                        )
+        job = self.runtime.run(program_id="circuit-runner-qasm3",
+                               options=options,
+                               inputs=program_inputs,
+                               result_decoder=RunnerResult,
+                               )
         self.log.debug("Job ID: %s", job.job_id())
         job.wait_for_final_state()
         self.assertEqual(job.status(), JobStatus.DONE, job.error_message())
+
+    def test_sim_single_str(self):
+        """Test the program on a simulator using a single QASM3 string."""
+        result = self._run_program()[0]
+        self.assertIsInstance(result, dict)
+        self.assertEqual(len(list(result.values())[0]), 2)  # 2 classical bits
+
+    def test_sim_single_str_shots(self):
+        """Test the program on a simulator with multiple shots."""
+        num_shots = 3
+        result = self._run_program(run_config={"shots": num_shots})
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), num_shots)
+        for shot_res in result:
+            self.assertEqual(len(list(shot_res.values())[0]), 2)  # 2 classical bits
+
+    def test_sim_single_str_args(self):
+        """Test the program on a simulator with args."""
+        result = self._run_program(
+            circuits=QASM3_STR_WITH_ARGS, qasm3_args={"flip": 1})
+        self.assertTrue(list(result[0].values())[0], result)
+
+    def test_sim_single_str_multi_args(self):
+        """Test the program on a simulator with multiple args."""
+        result = self._run_program(
+            circuits=QASM3_STR_WITH_ARGS, qasm3_args=[{"flip": 1}, {"flip": 0}])
+        self.assertTrue(list(result[0][0].values())[0], result)
+        self.assertFalse(list(result[1][0].values())[0], result)
+
+    def test_sim_single_str_multi_args_shot(self):
+        num_shots = 3
+        result = self._run_program(
+            circuits=QASM3_STR_WITH_ARGS,
+            qasm3_args=[{"flip": 1}, {"flip": 0}],
+            run_config={"shots": num_shots})
+        self.log.debug("test_sim_single_str_multi_args_shot result=%s", result)
+        for idx, arg_result in enumerate(result):
+            self.assertEqual(len(arg_result), num_shots)
+            expected = idx == 0
+            for shot_result in arg_result:
+                self.assertEqual(expected, list(shot_result.values())[0])
+
+    def _run_program(
+            self,
+            circuits=None,
+            qasm3_args=None,
+            run_config=None,
+            backend="simulator_qasm3",
+            block_for_result=True
+    ):
+        """Run the circuit-runner-qasm3 program.
+
+        Args:
+            circuits: Circuit(s) to run. Default is self.qasm3_str.
+            qasm3_args: Args to pass to the QASM3 program.
+            run_config: Execution time configuration.
+            backend: Backend to use.
+            block_for_result: Whether to block for result.
+
+        Returns:
+            Job result if `block_for_result` is ``True``. Otherwise the job.
+        """
+        circuits = circuits or QASM3_STR
+        program_inputs = {
+            "circuits": circuits
+        }
+        if qasm3_args:
+            program_inputs["qasm3_args"] = qasm3_args
+        if run_config:
+            program_inputs["run_config"] = run_config
+        options = {"backend_name": backend}
+
+        job = self.runtime.run(program_id=self.program_id,
+                               options=options,
+                               inputs=program_inputs
+                               )
+        self.log.debug("Job ID: %s", job.job_id())
+        if block_for_result:
+            return job.result()
+        return job
+
+    @classmethod
+    def _find_program_id(cls, program_name):
+        """Find ID of the program."""
+        for pgm in cls.runtime.programs(refresh=True):
+            if pgm.name == program_name:
+                return pgm.program_id
+        raise ValueError("Unable to find ID for program %s", program_name)
