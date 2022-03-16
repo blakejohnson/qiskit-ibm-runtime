f48538b5fd8b1f4f52794a72bf6e443b0e39232f
jessieyu <jessieyu@us.ibm.com>
2022-03-16 17:39:27 -0400

    update iqx primitives

--- iqx_programs/estimator.py
+++ iqx_programs/estimator.py
@@ -1,53 +1,60 @@
-from dataclasses import asdict
-
-import numpy as np
-from mthree.utils import final_measurement_mapping
-from qiskit.evaluators.backends import ReadoutErrorMitigation
-from qiskit.quantum_info import SparsePauliOp
+from qiskit_primitives.estimator import Estimator
 
 
-def main(backend, user_messenger, **kwargs):
-    state = kwargs.pop("circuit")
-    observable = kwargs.pop("observable")
-    if isinstance(observable, list):
-        observable = SparsePauliOp.from_list(observable)
-    parameters = kwargs.pop("parameters")
-    class_name = kwargs.pop("class_name", "PauliExpectationValue")
-    transpile_options = kwargs.pop("transpile_options", {})
-    run_options = kwargs.pop("run_options", {})
-    measurement_error_mitigation = kwargs.pop("measurement_error_mitigation", False)
+def main(
+        backend,
+        user_messenger,
+        circuits,
+        circuit_indices,
+        observables,
+        observable_indices,
+        parameters=None,
+        parameter_values=None,
+        skip_transpilation=False,
+        run_options=None,
+):
+    """Estimator primitive.
 
-    try:
-        if class_name == "PauliExpectationValue":
-            from qiskit.evaluators import PauliExpectationValue as Evaluator
+    Args:
+        backend: Backend to run the circuits.
+        user_messenger: Used to communicate with the user.
+        circuits: Quantum circuits that represent quantum states
+        observables: Observables.
+        parameters: Parameters of quantum circuits, specifying the order in which values
+            will be bound.
+        circuit_indices: List of circuit indices.
+        observable_indices: List of observable indices.
+        parameter_values: Concrete parameters to be bound.
+        skip_transpilation: Skip transpiling of circuits, default=False.
+        run_options: Execution time options.
 
-            if measurement_error_mitigation:
-                expval = Evaluator(state, observable, backend)
-                expval.set_transpile_options(**transpile_options)
-                mapping = final_measurement_mapping(expval.transpiled_circuits[0])
-                backend = ReadoutErrorMitigation(
-                    backend=backend,
-                    mitigation="mthree",
-                    refresh=1800,  # refresh the calibration data every 1800 seconds
-                    shots=8192,  # use M3's default shot number
-                    qubits=list(mapping),
-                )
-            else:
-                # use backend as is
-                pass
-        elif class_name == "ExactExpectationValue":
-            # Note: ExactExpectationValue works only with Aer backend
-            from qiskit.evaluators import ExactExpectationValue as Evaluator
-    except ModuleNotFoundError:
-        raise RuntimeError("You are not authorized to use this program.")
+    Returns: Expectation values and metadata.
 
-    expval = Evaluator(state, observable, backend)
-    expval.set_transpile_options(**transpile_options)
-    expval.set_run_options(**run_options)
-    result = expval.evaluate(parameters)
+    """
+    if len(circuit_indices) != len(observable_indices):
+        raise ValueError("The length of circuit_indices must "
+                         "match the length of observable_indices.")
+    if parameter_values and len(parameter_values) != len(circuit_indices):
+        raise ValueError("The length of parameter_values must "
+                         "match the length of circuit_indices")
 
-    ret = {
-        key: val.tolist() if isinstance(val, np.ndarray) else val
-        for key, val in asdict(result).items()
-    }
-    user_messenger.publish(ret, final=True)
+    estimator = Estimator(
+        backend=backend,
+        circuits=circuits,
+        observables=observables,
+        parameters=parameters,
+        skip_transpilation=skip_transpilation
+    )
+    run_options = run_options or {}
+    shots = run_options.get("shots") or backend.options.shots
+    raw_result = estimator(
+        circuit_indices=circuit_indices,
+        observable_indices=observable_indices,
+        parameter_values=parameter_values,
+        **run_options)
+
+    result = raw_result.__dict__
+    for metadata in result["metadata"]:
+        metadata["shots"] = shots
+
+    return result
